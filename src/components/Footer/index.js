d670fecec9ebdacbc5b1c36e95599fb68228ec29
jpd61 <jpd@dewoodyoil.com>
2020-08-02 15:59:50 -0500

    refactor and add more sites

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,6 +8,9 @@ function Footer() {
         <a href="https://twitter.com/jpdewoody"><img src="https://img.icons8.com/color/48/000000/twitter.png" alt="Twitter" class="icon"/></a>
         <a href="https://stackoverflow.com/users/12976894/jpd61"><img src="https://img.icons8.com/color/48/000000/stackoverflow.png" alt="Stack Overflow" class="icon"/></a>
         <p>
+            Made with <img id="react-icon" src="https://img.icons8.com/color/48/000000/react-native.png" alt="React"/>
+        </p>
+        <p>
         &copy; Joseph DeWoody
         </p>
     </div>
