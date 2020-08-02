3ce1953ac6a1b6b8acf38ff107d393cca5f07f79
jpd61 <jpd@dewoodyoil.com>
2020-08-02 18:25:08 -0500

    refactor

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -18,7 +18,7 @@ function Portfolio () {
 
           <Wrapper id="card-data">
             {portfolio.map((project) => (
-              <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} skills={project.skills}/>
+              <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
             ))}
           </Wrapper>
         </section>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,8 +14,8 @@ function ProjectCards(props) {
               <a href={props.github}><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Repository" id="project-icon"/></a> 
               <a href={props.deploy}><img src="https://img.icons8.com/color/48/000000/monitor.png" alt="Deployed Site" id="project-icon"/></a> 
               </p>
-              <p id="skills">
-                    ({props.skills})
+              <p id="topics">
+                    ({props.topics})
                 </p>
         </div>
       </div>
